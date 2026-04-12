0dc844cf9f9f7eb28ce21038f01acfbb579ffb30
vidaljorg-lang <vidaljorg@gmail.com>
2026-04-11 21:37:08 -0400

    Add NotFound component for 404 error page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>404</h1>
+      <p>Página no encontrada</p>
+    </div>
+  );
+};
+
+export default NotFound;
