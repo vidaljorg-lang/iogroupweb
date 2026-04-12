66cc96d9504bd84ac159e59871c2fb75b9664bda
vidaljorg-lang <vidaljorg@gmail.com>
2026-04-11 21:34:42 -0400

    Add Index component for the main page

---
+++ src/pages/Index.tsx
@@ -0,0 +1,10 @@
+const Index = () => {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>IO Group Web</h1>
+      <p>Proyecto funcionando en Vercel 🚀</p>
+    </div>
+  );
+};
+
+export default Index;
