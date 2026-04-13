4e69c853ffda536e4cf78154aa18e292c9fd2d34
vidaljorg-lang <vidaljorg@gmail.com>
2026-04-12 21:08:27 -0400

    Create Proyectos component for project display

---
+++ src/pages/Proyectos.tsx
@@ -0,0 +1,10 @@
+const Proyectos = () => {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Proyectos IO Group</h1>
+      <p>Aquí iremos mostrando nuestros videos y casos.</p>
+    </div>
+  );
+};
+
+export default Proyectos;
